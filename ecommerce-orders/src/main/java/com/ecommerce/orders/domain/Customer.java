24bcf536df2cb9a16d6dbcb0abfabd046c7e6dbd
rajeev <rajeev.ilavala@gmail.com>
2021-01-11 08:30:14 -0500

    created and modified POJO classes

---
+++ ecommerce-orders/src/main/java/com/ecommerce/orders/domain/Customer.java
@@ -0,0 +1,47 @@
+package com.ecommerce.orders.domain;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table
+@Entity(name = "customer")
+public class Customer implements Serializable{
+	@GeneratedValue
+    @Column(name = "c_id")
+    @Id
+    private UUID id;
+
+    private String name;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+	
+
+}
--- ecommerce-orders/src/main/java/com/ecommerce/orders/domain/Order.java
+++ ecommerce-orders/src/main/java/com/ecommerce/orders/domain/Order.java
@@ -1,34 +1,151 @@
 package com.ecommerce.orders.domain;
 
-public class Order {
-	private int id;
-	private String name;
-	private int cost;
-	
-	
-	public Order(int id,String name, int cost) {
-		this.id = id;
-		this.name=name;
-		this.cost = cost;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getCost() {
-		return cost;
-	}
-	public void setCost(int cost) {
-		this.cost = cost;
-	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	
 
+import java.io.Serializable;
+import java.util.List;
+import java.util.UUID;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "orders")
+public class Order implements Serializable{
+	
+	@Id
+    @Column(name = "order_id")
+    @GeneratedValue
+    private UUID id;
+    private OrderStatus status;
+    @OneToOne(cascade = CascadeType.ALL)
+    private Customer customer;
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "orderId")
+    private List<Item> items;
+    private long subTotal;
+    private double tax;
+    private long shipmentCharges;
+    private double total;
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "orderId")
+    private List<Payment> payments;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Address shippingAddress;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Address billingAddress;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public long getSubTotal() {
+        return subTotal;
+    }
+
+    public void setSubTotal(long subTotal) {
+        this.subTotal = subTotal;
+    }
+
+    public double getTax() {
+        return tax;
+    }
+
+    public void setTax(double tax) {
+        this.tax = tax;
+    }
+
+    public long getShipmentCharges() {
+        return shipmentCharges;
+    }
+
+    public void setShipmentCharges(long shipmentCharges) {
+        this.shipmentCharges = shipmentCharges;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public List<Payment> getPayments() {
+        return payments;
+    }
+
+    public void setPayments(List<Payment> payments) {
+        this.payments = payments;
+    }
+
+    public Address getShippingAddress() {
+        return shippingAddress;
+    }
+
+    public void setShippingAddress(Address shippingAddress) {
+        this.shippingAddress = shippingAddress;
+    }
+
+    public Address getBillingAddress() {
+        return billingAddress;
+    }
+
+    public void setBillingAddress(Address billingAddress) {
+        this.billingAddress = billingAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", status='" + status + '\'' +
+                ", customer=" + customer +
+                ", items=" + items +
+                ", SubTotal=" + subTotal +
+                ", tax=" + tax +
+                ", shipment_Charges=" + shipmentCharges +
+                ", total=" + total +
+                ", payments=" + payments +
+                ", shippingAddress=" + shippingAddress +
+                ", billingAddress=" + billingAddress +
+                '}';
+    }
+	
 }
